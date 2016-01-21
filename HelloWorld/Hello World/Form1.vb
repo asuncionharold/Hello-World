aab9c64c5e165f62c63f4ef52093397678b49a77
unknown <asuncionharold@gmail.com>
2016-01-21 23:09:50 +0800

    first commit

---
+++ HelloWorld/Hello World/Form1.vb
@@ -0,0 +1,12 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        My.Computer.Audio.Play(My.Resources.musicAbc, AudioPlayMode.BackgroundLoop)
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        My.Computer.Audio.Stop()
+        My.Computer.Audio.Play(My.Resources.button2, AudioPlayMode.WaitToComplete)
+        Form2.Show()
+    End Sub
+End Class
---
+++ HelloWorld/Hello World/Form2.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form2
+
+    Private Sub Form2_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        My.Computer.Audio.Play(My.Resources.button2, AudioPlayMode.WaitToComplete)
+        Timer1.Start()
+        Timer2.Start()
+    End Sub
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        If ProgressBar1.Value = 100 Then
+            Me.Close()
+            My.Computer.Audio.Play(My.Resources.musicAbc, AudioPlayMode.BackgroundLoop)
+        End If
+    End Sub
+
+    Private Sub Timer2_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer2.Tick
+        ProgressBar1.PerformStep()
+    End Sub
+End Class
